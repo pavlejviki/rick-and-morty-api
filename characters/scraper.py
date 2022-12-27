b6fab1717f645fb878bd4977918bf99986d88aec
Viktoria Pavlej <viktorialondon16@gmail.com>
2022-12-27 23:03:30 +0000

    Add CharacterSerializer

--- characters/scraper.py
+++ characters/scraper.py
@@ -9,9 +9,7 @@ def scrape_characters() -> list[Character]:
 
     characters = []
     while url_to_scrape is not None:
-        characters_response = requests.get(
-            url_to_scrape
-        ).json()
+        characters_response = requests.get(url_to_scrape).json()
 
         for character_dict in characters_response["results"]:
             characters.append(
@@ -26,3 +24,13 @@ def scrape_characters() -> list[Character]:
             )
         url_to_scrape = characters_response["info"]["next"]
     return characters
+
+
+def save_characters(characters: list[Character]) -> None:
+    for character in characters:
+        character.save()
+
+
+def sync_characters_with_api() -> None:
+    characters = scrape_characters()
+    save_characters(characters)
