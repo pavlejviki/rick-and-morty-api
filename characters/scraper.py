133da515f13999ef2ceef44a3caa57360ac83fe0
Viktoria Pavlej <viktorialondon16@gmail.com>
2022-12-27 22:33:40 +0000

    Add requests to requirements.txt

--- characters/scraper.py
+++ characters/scraper.py
@@ -5,19 +5,24 @@ from rick_and_morty_api import settings
 
 
 def scrape_characters() -> list[Character]:
-    characters_response = requests.get(
-        settings.RICK_AND_MORTY_API_CHARACTERS_URL
-    ).json()
+    url_to_scrape = settings.RICK_AND_MORTY_API_CHARACTERS_URL
+
     characters = []
-    for character_dict in characters_response["results"]:
-        characters.append(
-            Character(
-                api_id=character_dict["id"],
-                name=character_dict["name"],
-                status=character_dict["status"],
-                species=character_dict["species"],
-                gender=character_dict["gender"],
-                image=character_dict["image"],
+    while url_to_scrape is not None:
+        characters_response = requests.get(
+            url_to_scrape
+        ).json()
+
+        for character_dict in characters_response["results"]:
+            characters.append(
+                Character(
+                    api_id=character_dict["id"],
+                    name=character_dict["name"],
+                    status=character_dict["status"],
+                    species=character_dict["species"],
+                    gender=character_dict["gender"],
+                    image=character_dict["image"],
+                )
             )
-        )
-    return characters_response
+        url_to_scrape = characters_response["info"]["next"]
+    return characters
